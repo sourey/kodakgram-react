1e6c65e3a49cf60b5205c8c8b82d2ea3add068ed
Ashok Maharjan <sitaulasaurav@gmail.com>
2020-08-22 17:43:36 +0545

    minor bug fix

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Live At:
-https://kodakgram-react.herokuapp.com/
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -9,6 +9,7 @@ import {
   Popconfirm,
   Skeleton,
   Spin,
+  Empty,
 } from "antd";
 import {
   CommentOutlined,
@@ -192,23 +193,23 @@ class Feed extends Component {
         <Col md={6}></Col>
         <Col md={12}>
           <CreatePost getPosts={this.getPosts} />
-          {this.state.posts.length > 0 ? (
-            <InfiniteScroll
-              dataLength={this.state.posts.length} //This is important field to render the next data
-              children={this.state.posts}
-              next={this.getNextPosts}
-              hasMore={this.state.postsLength > this.state.posts.length}
-              loader={
-                <Spin
-                  style={{
-                    display: "inherit",
-                    textAlign: "center",
-                    marginRight: "120px",
-                    marginTop: "20px",
-                  }}
-                />
-              }
-              endMessage={
+          <InfiniteScroll
+            dataLength={this.state.posts.length} //This is important field to render the next data
+            children={this.state.posts}
+            next={this.getNextPosts}
+            hasMore={this.state.postsLength > this.state.posts.length}
+            loader={
+              <Spin
+                style={{
+                  display: "inherit",
+                  textAlign: "center",
+                  marginRight: "120px",
+                  marginTop: "20px",
+                }}
+              />
+            }
+            endMessage={
+              this.state.posts.length > 0 ? (
                 <p
                   style={{
                     textAlign: "center",
@@ -218,174 +219,160 @@ class Feed extends Component {
                 >
                   <b>Yay! You have seen it all</b>
                 </p>
-              }
-            >
-              {!this.state.postLoading ? (
-                <>
-                  {this.state.posts.map((post, idx) => (
-                    <>
-                      <Card
-                        key={post.postId}
-                        title={
-                          <>
-                            {post?.profilePictureUrl ? (
-                              <Avatar
-                                size={50}
-                                style={{
-                                  color: "#376e6f",
-                                  backgroundColor: "#376e6f",
-                                }}
-                                src={`${server}/files/${post.profilePictureUrl}`}
-                              />
-                            ) : (
-                              <Avatar
-                                size={40}
-                                icon={<UserOutlined />}
-                                style={{
-                                  color: "#FFFFFF",
-                                  backgroundColor: "#376e6f",
-                                }}
-                              />
-                            )}
-                            <Link
-                              to={{
-                                pathname: `/profile/${post.username}`,
-                                state: { userId: post.userId },
+              ) : null
+            }
+          >
+            {!this.state.postLoading ? (
+              <>
+                {this.state.posts.map((post, idx) => (
+                  <>
+                    <Card
+                      key={post.postId}
+                      title={
+                        <>
+                          {post?.profilePictureUrl ? (
+                            <Avatar
+                              size={50}
+                              style={{
+                                color: "#376e6f",
+                                backgroundColor: "#376e6f",
                               }}
-                              style={{ color: "black" }}
-                            >
-                              <span className="username">@{post.username}</span>
-                            </Link>
-                          </>
-                        }
-                        id={idx}
-                        className="feed-container"
-                        style={{ width: "450px" }}
-                        cover={
-                          <img
-                            alt={post.caption}
-                            src={`${server}/files/${post.image}`}
-                            onDoubleClick={() => {
-                              this.handleLike(post.postId, post.userId);
+                              src={`${server}/files/${post.profilePictureUrl}`}
+                            />
+                          ) : (
+                            <Avatar
+                              size={40}
+                              icon={<UserOutlined />}
+                              style={{
+                                color: "#FFFFFF",
+                                backgroundColor: "#376e6f",
+                              }}
+                            />
+                          )}
+                          <Link
+                            to={{
+                              pathname: `/profile/${post.username}`,
+                              state: { userId: post.userId },
                             }}
-                          />
-                        }
-                        actions={[
-                          <Row>
-                            <Col md={24}>
-                              <span
-                                style={{ marginRight: "10px" }}
-                                onClick={() => {
-                                  post.likedBy?.length !== 0 &&
-                                    this.setState(
-                                      {
-                                        openPostId: post.postId,
-                                        openPostUserId: post.userId,
-                                      },
-                                      () => {
-                                        this.setState({ modalVisible: true });
-                                      }
-                                    );
-                                }}
-                              >
-                                {post.likedBy?.length || 0} likes
-                              </span>
-                              {post.likedBy?.indexOf(
-                                localStorage.getItem("userId")
-                              ) !== -1 && post.likedBy !== undefined ? (
-                                <HeartFilled
-                                  style={{ color: "#eb2f96" }}
-                                  onClick={() =>
-                                    this.handleLike(post.postId, post.userId)
-                                  }
-                                />
-                              ) : (
-                                <HeartOutlined
-                                  style={{ color: "#eb2f96" }}
-                                  onClick={() =>
-                                    this.handleLike(post.postId, post.userId)
-                                  }
-                                />
-                              )}
-                            </Col>
-                          </Row>,
-                          <>
-                            <Row onClick={() => this.handleShowComments(idx)}>
-                              <Col md={12}>
-                                <span style={{ marginLeft: "80px" }}>
-                                  {post?.comments?.length}
-                                </span>
-                              </Col>
-                              <Col md={12}>
-                                <CommentOutlined
-                                  style={{ marginLeft: "50px" }}
-                                />
-                              </Col>
-                            </Row>
-                          </>,
-                          <>
-                            {post.userId === localStorage.getItem("userId") ? (
-                              <Popconfirm
-                                title="Are you sure to delete this post?"
-                                onConfirm={(e) =>
-                                  this.handleDeletePost(e, post.postId)
-                                }
-                                onCancel={this.cancelDelete}
-                                okText="Yes"
-                                cancelText="No"
-                                okButtonProps={{
-                                  style: {
-                                    backgroundColor: "#376e6f",
-                                    borderColor: "#376e6f",
-                                  },
-                                }}
-                              >
-                                <DeleteOutlined
-                                  style={{ marginLeft: "70px" }}
-                                />
-                              </Popconfirm>
-                            ) : null}
-                          </>,
-                          ,
-                        ]}
-                      >
-                        <Meta
-                          title={
-                            <span style={{ whiteSpace: "pre-line" }}>
-                              {post.caption}
-                            </span>
-                          }
-                          style={{
-                            fontWeight: "bold",
+                            style={{ color: "black" }}
+                          >
+                            <span className="username">@{post.username}</span>
+                          </Link>
+                        </>
+                      }
+                      id={idx}
+                      className="feed-container"
+                      style={{ width: "450px" }}
+                      cover={
+                        <img
+                          alt={post.caption}
+                          src={`${server}/files/${post.image}`}
+                          onDoubleClick={() => {
+                            this.handleLike(post.postId, post.userId);
                           }}
                         />
-                      </Card>
-                      <Comments
-                        id={idx}
-                        postId={post.postId}
-                        comments={post?.comments}
-                        showComments={post.showComments}
-                        handleComment={this.handleComment}
-                        handleDeleteComment={this.handleDeleteComment}
+                      }
+                      actions={[
+                        <Row>
+                          <Col md={24}>
+                            <span
+                              style={{ marginRight: "10px" }}
+                              onClick={() => {
+                                post.likedBy?.length !== 0 &&
+                                  this.setState(
+                                    {
+                                      openPostId: post.postId,
+                                      openPostUserId: post.userId,
+                                    },
+                                    () => {
+                                      this.setState({ modalVisible: true });
+                                    }
+                                  );
+                              }}
+                            >
+                              {post.likedBy?.length || 0} likes
+                            </span>
+                            {post.likedBy?.indexOf(
+                              localStorage.getItem("userId")
+                            ) !== -1 && post.likedBy !== undefined ? (
+                              <HeartFilled
+                                style={{ color: "#eb2f96" }}
+                                onClick={() =>
+                                  this.handleLike(post.postId, post.userId)
+                                }
+                              />
+                            ) : (
+                              <HeartOutlined
+                                style={{ color: "#eb2f96" }}
+                                onClick={() =>
+                                  this.handleLike(post.postId, post.userId)
+                                }
+                              />
+                            )}
+                          </Col>
+                        </Row>,
+                        <>
+                          <Row onClick={() => this.handleShowComments(idx)}>
+                            <Col md={12}>
+                              <span style={{ marginLeft: "80px" }}>
+                                {post?.comments?.length}
+                              </span>
+                            </Col>
+                            <Col md={12}>
+                              <CommentOutlined style={{ marginLeft: "50px" }} />
+                            </Col>
+                          </Row>
+                        </>,
+                        <>
+                          {post.userId === localStorage.getItem("userId") ? (
+                            <Popconfirm
+                              title="Are you sure to delete this post?"
+                              onConfirm={(e) =>
+                                this.handleDeletePost(e, post.postId)
+                              }
+                              onCancel={this.cancelDelete}
+                              okText="Yes"
+                              cancelText="No"
+                              okButtonProps={{
+                                style: {
+                                  backgroundColor: "#376e6f",
+                                  borderColor: "#376e6f",
+                                },
+                              }}
+                            >
+                              <DeleteOutlined style={{ marginLeft: "70px" }} />
+                            </Popconfirm>
+                          ) : null}
+                        </>,
+                        ,
+                      ]}
+                    >
+                      <Meta
+                        title={
+                          <span style={{ whiteSpace: "pre-line" }}>
+                            {post.caption}
+                          </span>
+                        }
+                        style={{
+                          fontWeight: "bold",
+                        }}
                       />
-                    </>
-                  ))}
-                </>
-              ) : (
-                <Spin className="spin-center" size="large" />
-              )}
-            </InfiniteScroll>
-          ) : (
-            <p
-              style={{
-                textAlign: "center",
-                marginRight: "25%",
-                fontWeight: "bold",
-              }}
-            >
-              Server down :(
-            </p>
-          )}
+                    </Card>
+                    <Comments
+                      id={idx}
+                      postId={post.postId}
+                      comments={post?.comments}
+                      showComments={post.showComments}
+                      handleComment={this.handleComment}
+                      handleDeleteComment={this.handleDeleteComment}
+                    />
+                  </>
+                ))}
+              </>
+            ) : (
+              <Spin className="spin-center" size="large" />
+            )}
+          </InfiniteScroll>
         </Col>
         <Col md={6}>
           <Followers />
